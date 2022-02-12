0b934156cddef19633dee17b35787de70b5028fc
almahmoud <mahmoudalexandru@gmail.com>
2022-02-11 23:42:59 -0500

    fixes

--- commit.sh
+++ commit.sh
@@ -5,11 +5,11 @@ mkdir -p lists
 touch lists/todo
 
 while [ -s lists/todo ]; do
-	git config --local user.email "action@github.com"
-	git config --local user.name "GitHub Action"
-	git add .
-	git commit -m "Periodic commit"
-	git push
-	sleep 300
+    git config --local user.email "action@github.com"
+    git config --local user.name "GitHub Action"
+    git add .
+    git commit -m "Periodic commit"
+    git push
+    sleep 300
 done
 
