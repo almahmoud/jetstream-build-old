ef82d746fff3754354168ae7adbd552d5b543f13
almahmoud <mahmoudalexandru@gmail.com>
2022-02-14 16:48:59 -0500

    Add monitoring and run from script

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bash -c "bash dispatch_loop.sh -n $NAMESPACE -c $PVC &
+sleep 5 && bash monitor_pods.sh &
+sleep 30 && bash cleanup_loop.sh -n $NAMESPACE -c $PVC &
+sleep 60 && bash commit.sh &
+sleep 60 && bash update_lists.sh"
