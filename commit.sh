87f14b95d7c7279dfe16fa5d0518fe1ec01a1676
almahmoud <mahmoudalexandru@gmail.com>
2022-02-14 15:37:29 -0500

    Fix brain fart

--- commit.sh
+++ commit.sh
@@ -5,11 +5,11 @@ touch lists/todo
 
 while [ -s lists/todo ]; do
     git config --local user.email "action@github.com"
-	git config --local user.name "GitHub Action"
-	git commit -m "Periodic commit" || true
+    git config --local user.name "GitHub Action"
+    git commit -m "Periodic commit" || true
     git add packages.json || true
     git add lists || true
-	git push || true
-	sleep 300
+    git push || true
+    sleep 300
 done
 
--- update_lists.sh
+++ update_lists.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
-while getopts "n:c:" flag
-do
-    case "${flag}" in
-        n) namespace=${OPTARG};;
-        c) claim=${OPTARG};;
-    esac
-done
-
-
-if [ -z "$namespace" ];
-    then echo "A namespace must be specific with eg: -n myinitials-mynamespace";
-    exit;
-fi
-
-
-if [ -z "$claim" ];
-    then echo "A persistent volume claim for persisting R libraries must be specific with eg: -c bioconductor-pvc";
-    exit;
-fi
 
 while [ -s lists/todo ]; do
-    python check_job_status.py -p $pkg -n $namespace -s manifests/$pkg/status -l manifests/$pkg/log
+	python update_lists.py -j packages.json -t lists/todo -d lists/done -r lists/removed -f lists/failed -s lists/skipped
 done
 
