3d95a8eac77d4c9a9006cd3260473ab425508156
almahmoud <mahmoudalexandru@gmail.com>
2022-02-15 14:17:33 -0500

    Continue

--- utils.sh
+++ utils.sh
@@ -4,3 +4,7 @@ kubectl get jobs -n newbuild --no-headers -o custom-columns=":metadata.name" | x
 
 # cleanup completed
 kubectl get jobs -n newbuild --no-headers | grep 1/1 | awk '{print $1}' | xargs kubectl delete -n newbuild job
+
+
+# All done PACKAGE NAMES
+kubectl get jobs -n newbuild --no-headers | grep 1/1 | awk '{print $1}' | xargs kubectl get -n newbuild --no-headers -o custom-columns=":spec.template.spec.containers[0].args" job | awk -F'"' '{print $2}' > lists/done
