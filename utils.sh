e102763fbf3d6583ffccaf1a3f6df20cce30c44f
almahmoud <mahmoudalexandru@gmail.com>
2022-02-14 23:28:32 -0500

    Force again

--- utils.sh
+++ utils.sh
@@ -1,6 +1,6 @@
 #cleanup all jobs
 
-kubectl get jobs -n testbuild --no-headers -o custom-columns=":metadata.name" | xargs kubectl delete -n testbuild job
+kubectl get jobs -n newbuild --no-headers -o custom-columns=":metadata.name" | xargs kubectl delete -n newbuild job
 
 # cleanup completed
-kubectl get jobs -n testbuild --no-headers | grep 1/1 | awk '{print $1}' | xargs kubectl delete -n testbuild job
+kubectl get jobs -n newbuild --no-headers | grep 1/1 | awk '{print $1}' | xargs kubectl delete -n newbuild job
